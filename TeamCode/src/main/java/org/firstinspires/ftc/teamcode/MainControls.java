a31e2458bfd98b109c6a5d83208023fd196cdf49
myles <mylestg@icloud.com>
2021-02-07 11:11:50 -0500

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainControls.java
@@ -37,21 +37,10 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-/**
- * This OpMode ramps a single motor speed up and down repeatedly until Stop is pressed.
- * The code is structured as a LinearOpMode
- *
- * This code assumes a DC motor configured with the name "left_drive" as is found on a pushbot.
- *
- * INCREMENT sets how much to increase/decrease the power each cycle
- * CYCLE_MS sets the update period.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-@TeleOp(name = "Concept: Ramp Motor Speed", group = "Concept")
+
+@TeleOp(name = "Main Control", group = "Concept")
 //@Disabled
-public class ConceptRampMotorSpeed extends LinearOpMode {
+public class MainControls extends LinearOpMode {
 
     static final double INCREMENT   = 0.05;     // amount to ramp motor each CYCLE_MS cycle
     static final int    CYCLE_MS    =   50;     // period of each cycle
@@ -68,8 +57,6 @@ public class ConceptRampMotorSpeed extends LinearOpMode {
     public void runOpMode() {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        // Connect to motor (Assume standard left wheel)
-        // Change the text in quotes to match any motor name on your robot.
         motorRB = hardwareMap.get(DcMotor.class, "RB");
         motorRF = hardwareMap.get(DcMotor.class, "RF");
 
@@ -90,38 +77,14 @@ public class ConceptRampMotorSpeed extends LinearOpMode {
             powerx = gamepad1.left_stick_x;
             powerw = gamepad1.right_stick_x;
 
-            //powerx += powerw;
-            //powery += powerw;
-            // Ramp the motors, according to the rampUp variable.
-            /* if (rampUp) {
-                // Keep stepping up until we hit the max value.
-                power += INCREMENT ;
-                if (power >= input ) {
-                    power = input;
-                   //rampUp = !rampUp;   // Switch ramp direction
-                }
-            }
-            else {
-                // Keep stepping down until we hit the min value.
-                power -= INCREMENT ;
-                if (power <= MAX_REV ) {
-                    power = MAX_REV;
-                    rampUp = !rampUp;  // Switch ramp direction
-                }
-            } */
 
-            // Display the current value
             telemetry.addData("1   Powery", "%5.2f", powery);
             telemetry.addData("2   Powerx", "%5.2f", powerx);
             telemetry.addData("3   Powerw", "%5.2f", powerw);
 
-            //telemetry.addData("2 LB Motor Power", "%5.2f", power);
-            //telemetry.addData("3 LF Motor Power", "%5.2f", power);
-            //telemetry.addData("4 RF Motor Power", "%5.2f", power);
-            //telemetry.addData(">", "Press Stop to end test." );
+
             telemetry.update();
 
-            // Set the motor to the new power and pause;
             motorRB.setPower(-powery + powerx - powerw);
             telemetry.addData("4   RightBack", "%5.2f", -powery + powerx - powerw);
 
